c5eec668e29496520967d13b7a203c9f2d43fb4c
Anthony Eden <anthonyeden@gmail.com>
2011-10-06 15:15:21 +0200

    A better status expectation

--- sinatra_address_book/features/step_definitions/api_steps.rb
+++ sinatra_address_book/features/step_definitions/api_steps.rb
@@ -23,7 +23,13 @@ When /^I send a DELETE request to "([^"]*)"$/ do |path|
 end
 
 Then /^the response status should be "([^"]*)"$/ do |status|
-  last_response.status.should eq(status.to_i), "response status code did not match: expected #{status} was #{last_response.status}\n#{last_response.body}"
+  begin
+    last_response.status.should eq(status.to_i)
+  rescue RSpec::Expectations::ExpectationNotMetError => e
+    puts "Response body:"
+    puts last_response.body
+    raise e 
+  end
 end
 
 Then /^the response body should be a JSON representation of the (\w+)$/ do |model|
