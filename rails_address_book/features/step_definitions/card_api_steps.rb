41a0b199cd782221eb99cf064669a92f2651c441
Anthony Eden <anthonyeden@gmail.com>
2011-10-07 02:01:43 +0200

    unroll that a bit

--- rails_address_book/features/step_definitions/card_api_steps.rb
+++ rails_address_book/features/step_definitions/card_api_steps.rb
@@ -1,5 +1,7 @@
 Then /^the first name in the JSON representation of the card should be "([^"]*)"$/ do |first_name|
-  JSONSelect('.first_name').match(JSON.parse(last_response.body)).should eq(first_name)
+  json = JSON.parse(last_response.body)
+  match = JSONSelect('.first_name').match(json)
+  match.should eq(first_name)
 end
 
 Then /^the response body should be a JSON v(\d+) representation of the Card$/ do |version|
